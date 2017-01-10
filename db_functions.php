490417b55b2a02a0e0abd3ab4ad8c19e733a261e
kartik chauhan <chauhan.kartik25@gmail.com>
2017-01-11 01:18:54 +0530

    classes and functions added for login and registration - still need to check if it's working fine - materialize frontend still remaining

--- db_functions.php
+++ db_functions.php
@@ -12,7 +12,7 @@
 			$db = new db_connect;
 		}
 
-		public function Register($name, $email, $username, $password)
+		public function Register($name, $username, $email, $password)
 		{
 			if(isUserExists($email))
 			{
@@ -48,4 +48,73 @@
 		}
 	}
 
+	function validateDataRegistration()
+	{
+		
+		$name = $_POST['name'];
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$password = $email = $_POST['password'];
+		$confirm_password = $email = $_POST['confirm_password'];
+		if(empty($name))
+			throw new Exception("Enter your name first before submitting");
+
+		if(empty($username))
+			throw new Exception("Enter a username first before submitting");
+
+		if(empty($email))
+			throw new Exception("Enter your emailId first before submitting");
+		$username = $_POST['username'];
+
+		if(empty($password))
+			throw new Exception("Enter a password first before submitting");
+
+		if(empty($confirm_password))
+			throw new Exception("Please enter your password again");
+
+		if($confirm_password!=$password)
+			throw new Exception("Password doesn't match. Please try again");
+
+		try
+		{
+			$registerUser = new db_functions;
+			$registerUser->Register($name, $username, $email, $password);
+		}
+		catch(Exception $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	function validateDataLogin()
+	{
+		$email = $_POST['email'];
+		$password = $email = $_POST['password'];
+
+		if(empty($email))
+			throw new Exception("Enter your emailId to Login");
+
+		if(empty($password))
+			throw new Exception("Please enter your password to Login");
+
+		try
+		{
+			$loginUser = new db_functions;
+			$loginUser->Login($email, $password);
+		}
+		catch(Exception $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	if(isset($_POST['register']))
+	{
+		validateDataRegistration();
+	}
+
+	if(isset($_POST['login']))
+	{
+		validateDataLogin();
+	}
 ?>
